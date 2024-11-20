ac6c109eb299e62a651c50e2a4cb8486db7b54db
Filip Stolín <stolin.f@gmail.com>
2024-11-20 18:05:38 +0100

    Extracting console input to interface

---
+++ SyncTask/Services/ConsoleUserInputChecker.cs
@@ -0,0 +1,23 @@
+﻿using SyncTask.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTask.Services
+{
+    internal class ConsoleUserInputChecker : IUserInputChecker
+    {
+        public bool UserPressedKey()
+        {
+            if (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+                return true;
+            }
+            return false;
+        }
+
+    }    
+}
